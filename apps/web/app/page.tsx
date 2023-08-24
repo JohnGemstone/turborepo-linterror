04becc65d7b0668d24f50565ae88e33f03196b80
Jim Johnston <james.johnston.uk@gmail.com>
2023-08-24 20:47:27 +0100

    add env to turbo config, inject into web homepage

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,12 @@
 import { Button, Header } from "ui";
 
+const foo = process.env.NEXT_PUBLIC_TEST ?? "no env";
+
 export default function Page(): JSX.Element {
   return (
     <>
       <Header text="Web" />
+      <p>{foo}</p>
       <Button />
     </>
   );
